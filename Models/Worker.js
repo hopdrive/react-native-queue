cc81ef4409af6ec462efe8b3c3268f432ec92486
RobNewton <robbienewton@gmail.com>
2023-08-22 10:56:57 -0400

    First attempt wrapping worker job in worklet

--- Models/Worker.js
+++ Models/Worker.js
@@ -1,3 +1,5 @@
+import { Worklets } from "react-native-worklets-core";
+
 /**
  *
  * Worker Model
@@ -33,7 +35,9 @@ export default class Worker {
   addWorker(jobName, worker, options = {}) {
     // Validate input.
     if (!jobName || !worker) {
-      throw new Error('Job name and associated worker function must be supplied.');
+      throw new Error(
+        "Job name and associated worker function must be supplied."
+      );
     }
 
     // Attach options to worker
@@ -43,7 +47,7 @@ export default class Worker {
       onSuccess: options.onSuccess || null,
       onFailure: options.onFailure || null,
       onFailed: options.onFailed || null,
-      onComplete: options.onComplete || null
+      onComplete: options.onComplete || null,
     };
 
     Worker.workers[jobName] = worker;
@@ -72,10 +76,22 @@ export default class Worker {
   getConcurrency(jobName) {
     // If no worker assigned to job name, throw error.
     if (!Worker.workers[jobName]) {
-      throw new Error('Job ' + jobName + ' does not have a worker assigned to it.');
+      throw new Error(
+        "Job " + jobName + " does not have a worker assigned to it."
+      );
     }
 
     return Worker.workers[jobName].options.concurrency;
+  }
+
+  /**
+   *
+   * Run job in worklet
+   *
+   */
+  startInWorklet(fn) {
+    "worklet";
+    fn();
   }
 
   /**
@@ -90,7 +106,9 @@ export default class Worker {
   async executeJob(job) {
     // If no worker assigned to job name, throw error.
     if (!Worker.workers[job.name]) {
-      throw new Error('Job ' + job.name + ' does not have a worker assigned to it.');
+      throw new Error(
+        "Job " + job.name + " does not have a worker assigned to it."
+      );
     }
 
     // Data must be cloned off the realm job object for the timeout logic promise race.
@@ -103,11 +121,24 @@ export default class Worker {
     if (jobTimeout > 0) {
       let timeoutPromise = new Promise((resolve, reject) => {
         setTimeout(() => {
-          reject(new Error('TIMEOUT: Job id: ' + jobId + ' timed out in ' + jobTimeout  + 'ms.'));
+          reject(
+            new Error(
+              "TIMEOUT: Job id: " +
+                jobId +
+                " timed out in " +
+                jobTimeout +
+                "ms."
+            )
+          );
         }, jobTimeout);
       });
 
-      await Promise.race([timeoutPromise, Worker.workers[jobName](jobId, jobPayload)]);
+      const worklet = Worklets.createRunInContextFn(this.startInWorklet);
+
+      await Promise.race([
+        timeoutPromise,
+        worklet(() => Worker.workers[jobName](jobId, jobPayload)),
+      ]);
     } else {
       await Worker.workers[jobName](jobId, jobPayload);
     }
@@ -124,9 +155,15 @@ export default class Worker {
    */
   async executeJobLifecycleCallback(callbackName, jobName, jobId, jobPayload) {
     // Validate callback name
-    const validCallbacks = ['onStart', 'onSuccess', 'onFailure', 'onFailed', 'onComplete'];
+    const validCallbacks = [
+      "onStart",
+      "onSuccess",
+      "onFailure",
+      "onFailed",
+      "onComplete",
+    ];
     if (!validCallbacks.includes(callbackName)) {
-      throw new Error('Invalid job lifecycle callback name.');
+      throw new Error("Invalid job lifecycle callback name.");
     }
 
     // Fire job lifecycle callback if set.
